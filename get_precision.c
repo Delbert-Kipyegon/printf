0f25751cdfb17d26c3322a5a7e8ddea3d1d2e44c
Irwaresly <silviairware372@gmail.com>
2023-07-26 04:04:06 +0300

    Update get_precision.c
    PRINTF changes

--- get_precision.c
+++ get_precision.c
@@ -2,6 +2,7 @@
 
 /**
  * get_precision - Calculates the precision for printing
+ * Sly and Delbert
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  * @list: list of arguments.
